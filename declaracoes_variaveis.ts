7fc50e93e0c6aaef8bf6abbfbcb4946845e1554f
MichelecFernandes <michelecristinafr@gmail.com>
2026-03-17 20:41:30 -0300

    docs: methods for manipulating elements of a array

--- declaracoes_variaveis.ts
+++ declaracoes_variaveis.ts
@@ -29,3 +29,11 @@ let nomes: string[] = ["Maria", "João", "Samuel"];
 let primeiroNumero = numeros[0]; // Obtém o primeiro número (1)
 let segundoNome = nomes[1];      // Obtém o segundo nome ("João")
 
+// Manipular arrays
+
+// Pop():remove o último elemento de um array e retorna esse elemento.
+// Push():adiciona  um  ou  mais  elementos  ao  final  de  um  array  e  retorna  o tamanho desse array.
+// Shift():remove o primeiro elemento de um array e retorna esse elemento.
+// Unshift():adiciona  um  ou  mais  elementos  no  início  de  um  array  e  retorna  o tamanho deste array.
+// reduce():executa uma função reducer (fornecida por você) para cada elemento do array, resultando num único valor de retorno.
+// reverse():inverte os itens de um array. O primeiro elemento do array se torna o último e o último torna-se o primeiro
