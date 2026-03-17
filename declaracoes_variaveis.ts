d71910186e872e42c6bde4da20c61c07f7f01bf3
MichelecFernandes <michelecristinafr@gmail.com>
2026-03-17 20:22:36 -0300

    docs: file about variable declarations

---
+++ declaracoes_variaveis.ts
@@ -0,0 +1,6 @@
+// Variaveis com tipo explicitos
+let idade: number;
+idade = 30 // Valor atribuído é do tipo number
+
+// Variaveis com tipo implicitos 
+let nome = "João" // TS infere que o 'nome' é do tipo string
