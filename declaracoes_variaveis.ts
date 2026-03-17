0410b0c31969abf6adef82a1319040527c1497ce
MichelecFernandes <michelecristinafr@gmail.com>
2026-03-17 20:37:26 -0300

    docs: examples of the variable declarations

--- declaracoes_variaveis.ts
+++ declaracoes_variaveis.ts
@@ -1,6 +1,31 @@
 // Variaveis com tipo explicitos
-let idade: number;
-idade = 30 // Valor atribuído é do tipo number
+let idade1: number;
+idade1 = 30 // Valor atribuído é do tipo number
 
 // Variaveis com tipo implicitos 
-let nome = "João" // TS infere que o 'nome' é do tipo string
+let nome1 = "João" // TS infere que o 'nome' é do tipo string
+
+// Tipos basicos em TS
+
+// number: representa números inteiros ou de ponto flutuante. Exemplo
+const idade: number = 30;
+const altura: number = 1.75;
+
+// string: representa sequências de caracteres, como texto. Exemplo
+const nome: string = "Thamiris";
+const mensagem: string = "Olá, Thamiris!";
+
+// boolean: representa valores lógicos verdadeiro (true) ou falso (false). Exemplo
+const ativo: boolean = true;
+const isAdmin: boolean = false;
+
+// Declarando e manipulando um Array
+
+// Deve ser criado usando a notação de colchetes []
+let numeros: number[] = [1, 2, 3, 4, 5];
+let nomes: string[] = ["Maria", "João", "Samuel"];
+
+// Para acessar um informação do array é necessãrio passar a pósição do elemento
+let primeiroNumero = numeros[0]; // Obtém o primeiro número (1)
+let segundoNome = nomes[1];      // Obtém o segundo nome ("João")
+
